a8a9b04cfa2b9a2483502ca9992dad7418baca36
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2022-12-23 17:40:07 +0000

    add health check route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const { v4 } = require('uuid');
 
-app.get('/api', (req, res) => {
+app.get('/api/items', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
@@ -12,5 +12,9 @@ app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
+
+app.get('/api/health', (req, res) => {
+  res.status(200).end("OK");
+})
 
 module.exports = app;
